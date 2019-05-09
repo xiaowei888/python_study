e9697d23175e26dcd89c3e1a02cae32a4b814112
xiaowei888 <780746477@qq.com>
2019-05-09 17:40:28 +0800

    添加ro.com.google.clientidbase的打印，为了方便得知用的哪家mada

--- test_adb_1.py
+++ test_adb_1.py
@@ -2,7 +2,7 @@
 import os
 
 p1=os.popen("adb devices").read()
-print (p1)
+print (p1,end="")
 
 cmd="adb shell getprop "
 prop1="ro.product.vendor.model"
@@ -12,37 +12,39 @@ args = ("ro.product.vendor.model","ro.product.model",
         "ro.product.vendor.brand","ro.product.brand",
         "ro.vendor.build.fingerprint", "ro.build.fingerprint",
         "ro.product.vendor.name","ro.product.name","ro.build.product",
-        "ro.build.version.incremental","ro.com.google.gmsversion",
+        "ro.build.version.incremental","ro.com.google.gmsversion","ro.com.google.clientidbase"
         )
 count = 0
 result={}
 test_result=[]
 while(count < len(args)):
     pi = os.popen(cmd+args[count]).read()
-    print(args[count] + " = " + pi)
+    print(args[count] + " = " + pi,end="")
     result[args[count]] = pi
     count = count+1
+
+print("\033[1;31;0m=========================================================================================================\033[0m")
 
 count2=0
 while(count2<10):    
     if(result[args[count2]] == result[args[count2+1]]):
-        print(args[count2],"==",args[count2+1],"==",result[args[count2]])
+        print(args[count2],"==",args[count2+1],"==",result[args[count2]],end="")
         test_result.append("true");
     else:
-        print("\033[1;31;0m",args[count2],"!=",args[count2+1],"!=",result[args[count2]],result[args[count2+1]],"\033[0m")
+        print("\033[1;31;0m",args[count2],"!=",args[count2+1],"!=",result[args[count2]],result[args[count2+1]],"\033[0m",end="")
         test_result.append("false");
     count2 = count2 + 2    
             
 if(result[args[11]] == result[args[12]] == result[args[10]]):
     test_result.append("true");
-    print(args[11],"==",args[12],"==",args[10],"==",result[args[0]])
+    print(args[11],"==",args[12],"==",args[10],"==",result[args[0]],end="")
 else:
-    print("\033[1;31;0m",args[11],"!=",args[12],"!=",args[10],"\033[0m","\n")
+    print("\033[1;31;0m",args[11],"!=",args[12],"!=",args[10],"\033[0m","\n",end="")
     test_result.append("false");
     print(result[args[11]].strip(),result[args[12]].strip(),result[args[10]].strip(),"\n")
     
 if result["ro.build.version.incremental"].strip() in result["ro.build.fingerprint"].strip():###只有添加strip()去掉字符串中的空格才可准确比较
-    print("ro.build.version.incremental match ro.build.fingerprint")
+    print("ro.build.version.incremental match ro.build.fingerprint",end="")
     test_result.append("true");
 else:
     print("ro.build.version.incremental is not match ro.build.fingerprint")
